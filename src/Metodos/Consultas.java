bc011c1abe34a41daeb09e3fd6eb584d8b9efea3
a16alfonsofa <fonsi20fernandez@gmail.com>
2019-01-25 12:04:52 +0100

    Arreglos

--- src/Metodos/Consultas.java
+++ src/Metodos/Consultas.java
@@ -6,14 +6,11 @@ package Metodos;
  */
 public class Consultas {
 
-    public static void GastoClienteFechas() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
     public static void MAXGimCliente() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
-    
-    
+
+    public static void GastoClienteFechas() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
